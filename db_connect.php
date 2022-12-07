cb37805d68b1dde317a7d041255cc7f30dfda30a
phamxuantruong843 <91688786+phamxuantruong843@users.noreply.github.com>
2022-12-07 23:41:07 +0700

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+//code by Phạm Xuân Trường
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "quanlytiemnet";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+$conn -> set_charset('utf8');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
